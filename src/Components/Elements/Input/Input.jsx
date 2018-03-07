045ebf832bef891bfa482ad8bb1a54b6fa007f72
Artem <artem.studying@gmail.com>
2018-03-08 01:52:15 +0200

    all basic functional works

---
+++ src/Components/Elements/Input/Input.jsx
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import Btn from '../Button/Btn'
+
+class Input extends Component{
+	constructor(props){
+		super(props);
+		this.state = {value: props.value}
+		this.updateValue = this.updateValue.bind(this);
+	}
+	
+	updateValue(e){
+		this.props.updateValue(e);
+	}
+	
+	render(){
+		const { type, name , value , id  } = this.props;
+		return(
+			<div className={this.props.myStyleGroupe}>
+				<label className={this.props.myStyleL}>{this.props.nameL}</label>
+				<input
+					className={this.props.myStyleI}
+					id={id}
+					type={this.props.type}
+					name={this.props.name}
+					onChange = {this.updateValue}
+					value={this.state.value}
+				/>
+				<Btn 
+					BtnName={this.props.btnName} 
+					myStyle={this.props.btnStyle}
+					press={this.props.btnPress}/>
+			</div>
+		);
+	}
+
+}
+
+export default Input;
--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,47 +1,88 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import Style from './SearchPage.css'
 import Btn from '../Elements/Button/Btn'
+import Input from '../Elements/Input/Input'
 
 class SearchPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            inputValue: props.inputValue,
             searchData: []
         }
         this.getSearchData = this.getSearchData.bind(this);
+        this.changeInputValue = this.changeInputValue.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
     
     // componentWillMount(){
     //     this.getSearchData();
     // }
+    changeInputValue(e){
+        this.setState({inputValue: e.target.value})
+        console.log(this.state.inputValue);
+        
+    }
     getSearchData(){
-        let inputField = document.querySelector('.input');
+        // let inputField = document.querySelector('.input');
 
-        fetch('https://api.github.com/search/repositories?q='+inputField,
+        fetch('https://api.github.com/search/repositories?q='+this.state.inputValue,
             {   mode:'cors',
                 method: 'GET',
             })
         .then(res=>{
             res.json().then(res=>{
-                this.setState({searchData:res})
-                // console.log(res);
+                this.setState({searchData:res.items})
+                console.log(res);
             })
         })
     }
-    render(){
-        // let searchResult = this.state.searchData? this.state.searchData.item.map((item, i)=> {
 
-        // }):' ';
-        // console.log(searchResult);
+    deleteItem(id){
+        let arr = this.state.searchData;
+        arr.forEach((item,i) => {
+            if(item.id === id){
+                arr.splice(i, 1);
+            }
+        });
+        this.setState({searchData: arr})
+    }
+    
+    
+
+
+    render(){
+        
+        let requestItemList = this.state.searchData? this.state.searchData.map((item, i)=> {
+                        return(
+                            <div key={i} className='search-result-box'>
+                                <span className='result-item'>{item.name}</span>
+                                <span className='btn-delete' onClick={()=>{this.deleteItem(item.id)}}>Delete</span>
+                            </div>
+                        )
+        }):'';
         
         return(
-            <div>
-                <div className="input-group">
-                    <label>Search Request</label>
-                    <input type="text" placeholder="Search" />
-                    <Btn myStyle='btn' BtnName='Search' press={this.getSearchData}/>
+            
+            <div className='container'>
+                        <Input 
+                            myStyleGroupe="input-group"
+							type="text" 
+							name="Search"
+							myStyleI="form_input-AUTH"
+							nameL="Search Request"
+							myStyleL="form_label-AUTH" 
+							updateValue={this.changeInputValue} 
+                            value={this.state.inputValue}
+                            btnName="Search"
+                            btnStyle="btn"
+                            btnPress={this.getSearchData}
+
+						/>
+							
+                <div>
+                    {requestItemList}
                 </div>
-                <div> </div>
 
             </div>
         )
