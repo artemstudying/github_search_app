5ece10a6c590f38c0ac1480a2fbeede2d8b6d27c
Artem <artem.studying@gmail.com>
2018-03-09 20:10:06 +0200

    css

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -8,11 +8,13 @@ class SearchPage extends Component {
         super(props);
         this.state = {
             inputValue: props.inputValue,
-            searchData: []
+            searchData: [],
+            itemOfData: {}
         }
         this.getSearchData = this.getSearchData.bind(this);
         this.changeInputValue = this.changeInputValue.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
+        this.changeSelectedRepo = this.changeSelectedRepo.bind(this);
     }
     
     // componentWillMount(){
@@ -33,7 +35,11 @@ class SearchPage extends Component {
         .then(res=>{
             res.json().then(res=>{
                 this.setState({searchData:res.items})
-                console.log(res);
+                // this.setState({itemOfData: this.searchData[0]})
+                // let data = JSON.parse('res')
+                this.setState({itemOfData:res.items[0]})
+                // console.log(res)
+                console.log(res.items[0]);
             })
         })
     }
@@ -48,39 +54,66 @@ class SearchPage extends Component {
         this.setState({searchData: arr})
     }
     
-    
+    changeSelectedRepo(id){
+        this.state.searchData.forEach((item,i)=>{
+            if(item.id === id){
+                this.setState({itemOfData: item})
+            }
+        })
+    }
 
 
     render(){
+        console.log(this.state.itemOfData)
         
         let requestItemList = this.state.searchData? this.state.searchData.map((item, i)=> {
                         return(
                             <div key={i} className='search-result-box'>
-                                <span className='result-item'>{item.name}</span>
-                                <span className='btn-delete' onClick={()=>{this.deleteItem(item.id)}}>Delete</span>
+                                <span className='result-item'onClick={()=>{this.changeSelectedRepo(item.id)}}>{item.name}</span>
+                                <span className='btn btn-delete' onClick={()=>{this.deleteItem(item.id)}}>DELETE</span>
                             </div>
                         )
         }):'';
+
+
+
+        let selectedItem = this.state.itemOfData? 
+                                    <div className='git-page-info'>
+                                        <div className='name-of-repo'>
+                                            <span>Name of selected Repository:</span>
+                                            <h3>{this.state.itemOfData.name}</h3>
+                                        </div>
+                                        <a className='btn btn-repo' href={this.state.itemOfData.svn_url} target='_blank'>GitHub</a>
+                                    </div>
+                                :' ';
         
         return(
             
             <div className='container'>
+                <div className='header-wrap'>
+                    <header>
+                        <h1 className='logo'>
+                            GitHub Search
+                        </h1>
                         <Input 
-                            myStyleGroupe="input-group"
-							type="text" 
-							name="Search"
-							myStyleI="form_input-AUTH"
-							nameL="Search Request"
-							myStyleL="form_label-AUTH" 
-							updateValue={this.changeInputValue} 
-                            value={this.state.inputValue}
-                            btnName="Search"
-                            btnStyle="btn"
-                            btnPress={this.getSearchData}
-
-						/>
-							
-                <div>
+                        myStyleGroupe="input-group"
+                        type="text" 
+                        name="Search"
+                        myStyleI="form_input-AUTH"
+                        nameL="Search Request"
+                        myStyleL="form_label-AUTH" 
+                        updateValue={this.changeInputValue} 
+                        value={this.state.inputValue}
+                        btnName="Search"
+                        btnStyle="btn"
+                        btnPress={this.getSearchData}
+                        />    
+                    </header>
+                </div>
+                <div className='selected-item-wrap'>
+                    {selectedItem}
+                </div>
+                <div className='result-container'result>
                     {requestItemList}
                 </div>
 
